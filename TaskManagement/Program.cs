24f8e3c9c8a954bb2b4e3fb254ec49a3f8a84c8b
monarkkurosaki <monarkyadav@gmail.com>
2025-04-15 18:28:27 +0545

    Finally solved

--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        int indexOf;
         Console.WriteLine("welcome to the task manager");
         Console.WriteLine("Press any key to continue");
         Console.ReadKey();
@@ -16,30 +17,33 @@ class Program
         while (!userInput.Equals("4"))
         {
             Console.WriteLine("Enter 1 to enter task");
-            Console.WriteLine("Enter 2 if you want to view the tasks");
-            Console.WriteLine("Enter 3 if your want to remove the entry");
+            Console.WriteLine("Enter 2 if you want to remove the tasks");
+            Console.WriteLine("Enter 3 if your want to view the entry");
             Console.WriteLine("Enter 4 to exit the program");
             userInput = Console.ReadLine();
             
             if (userInput.Equals("1"))
             {
+                Console.Clear();
                 Console.Write("Enter the task you want to enter: ");
                 string taskToBeAdded = Console.ReadLine();
                 tasks.Add(taskToBeAdded);
                
             }else if (userInput.Equals("2"))
             {
+                Console.Clear();
                 Console.Write("Enter the task you want to delete: ");
                 string taskToBeDeleted = Console.ReadLine();
-                tasks.Remove(taskToBeDeleted);
+                indexOf = tasks.IndexOf(taskToBeDeleted);
+                tasks.RemoveAt(indexOf);
                 
             }else if (userInput.Equals("3"))
             {
+                Console.Clear();
                 foreach (var task in tasks)
                 {
                     Console.WriteLine(task);
                 }
-                
             }
             else
             {
--- TaskManagement/obj/Debug/net9.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagement/obj/Debug/net9.0/TaskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3010ef89982d6530d76d403cd7a66afa93b3bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26902a11df1e705a2cb22cc803b5a96045880a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
